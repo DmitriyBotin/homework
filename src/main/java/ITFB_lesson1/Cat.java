3d1dd7c8c060d4ca8cf2024ac5b2fcc07d16c538
botin <botindima@gmail.com>
2022-04-04 22:25:11 +0300

    lesson1

---
+++ src/main/java/ITFB_lesson1/Cat.java
@@ -0,0 +1,24 @@
+package ITFB_lesson1;
+
+public class Cat {
+    //private default protected public
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+
+    }
+
+    public void info() {
+        System.out.println(name + " " + color + " " + age);
+    }
+
+    public void meow() {
+        System.out.println(name + " meow");
+    }
+
+}
---
+++ src/main/java/ITFB_lesson1/MainApp.java
@@ -0,0 +1,13 @@
+package ITFB_lesson1;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Cat cat2 = new Cat("Murzik", "Black", 4);
+        Cat cat = new Cat("Barsik", "White", 2);
+
+
+
+        cat.info();
+        cat2.info();
+    }
+}
